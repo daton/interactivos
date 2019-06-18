c2c1ce4a8db173e8f7a21700fff8589f3f689d8d
Juan Campos <rapidclimate@gmail.com>
2019-06-18 11:28:00 -0500

    Add files via upload

---
+++ CasaControlador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CasaControlador : MonoBehaviour
+{
+
+    public InputField campo;
+    public void Salir()
+    {
+        //Para salir de la app
+        Application.Quit();
+    }
+    
+
+    public void calcular()
+    {
+
+
+
+    }
+
+}
